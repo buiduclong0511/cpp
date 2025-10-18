bba8cb1a0e4cbdcf26126a7148be8688c525f2ac
Long Bùi <longbui@Mac.lan>
2025-10-18 12:54:19 +0700

    commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    bool da_tot_nghiep = true;
+
+    if (da_tot_nghiep == true) {
+        cout << "Chuc mung ban da tot nghiep!" << endl;
+    } else {
+        cout << "Ban chua tot nghiep." << endl;
+    }
+
+    return 0;
+}
