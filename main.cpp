4d66d9b4237a4261d6b08aee598830d1e66c48ac
Long Bùi <longbui@Mac.lan>
2025-11-01 17:24:20 +0700

    Update doc

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    bool da_tot_nghiep = true;
-
-    if (da_tot_nghiep == true) {
-        cout << "Chuc mung ban da tot nghiep!" << endl;
-    } else {
-        cout << "Ban chua tot nghiep." << endl;
-    }
 
     return 0;
 }
