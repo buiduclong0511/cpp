59943044bd21d8e0bb717d291b123ec4be6fe1e3
Long <buiduclong0511@gmail.com>
2025-11-08 09:53:14 +0700

    Add .gitignore and documentation for C++ functions; update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void changeValue(int &a) {
+    a = 5;
+}
+
 int main() {
+
+    int a = 1;
+
+    changeValue(a);
+
+    cout << "Value of a: " << a << endl; // This will print 1, not 5
 
     return 0;
 }
