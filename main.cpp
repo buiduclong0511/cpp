da80f2076da61a48dd197d3e6622bee9553bb2f8
Long <buiduclong0511@gmail.com>
2025-11-29 14:28:12 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int x() {
-    cout << "1" << endl;
-    cout << "2" << endl;
+int reverseNumber(int n) {
+    int result = 0;
 
-    cout << "3" << endl;
+    while(n > 0) {
+        // Lấy ra chữ số cuối cùng bằng cách chia dư cho 10
+        int y = n % 10;
+        
+        // sau khi có chữ số cuối cùng thì nhân kết quả hiện tại với 10 
+        // sau đó cộng thêm chữ số cuối cùng vào hàng đơn vị
+        result = result * 10 + y;
+
+        // chia n cho 10 để xoá bớt 1 hàng chữ số đi
+        n /= 10;
+
+        // Cứ thế lặp lại cho đến khi n = 0
+    }
+
+    return result;
 }
 
 int main() {
-    x();
+    cout << reverseNumber(1230) << endl;
+    cout << reverseNumber(0) << endl;
+    cout << reverseNumber(231) << endl;
 
     return 0;
 }
